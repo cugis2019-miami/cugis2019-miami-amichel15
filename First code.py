4b2bcd2e2012a8dd10a9d834f4c4d136a85d03c4
amichel15 <52931977+amichel15@users.noreply.github.com>
2019-07-15 15:52:17 -0400

    Add files via upload

---
+++ First code.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("I am learning to code in python today")
+
+print(63)
+
+53
+
+"hello world"
+
+print("53")
+
+5*2
+
+5/2
+
+5**2
+
+(8/9)*3
+
+6/3 #division
+
+"hello" #greeting
+
+print("the division of 5/2 is", 5/2)
+
+#create function to sum any two numbers
+
+def sum(a,b):
+    sum = a + b 
+    print(sum)
+
+sum(6,2)
+
+#create function to sum any 3 numbers 
+
+def plus(a,b,c):
+    plus = a + b + c 
+    print(plus)
+    
+plus(6,2,3)
+
+#create a function to calculate the area of a triangle
+def triarea(b,h):
+    triarea = (1/2)*b*h
+    print(triarea)
+    
+triarea(6,2)
+
